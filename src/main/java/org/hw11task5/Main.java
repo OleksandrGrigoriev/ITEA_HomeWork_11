3e4f50981604398129b6bf885952100ddf06c0f6
OleksandrGrigoriev <alexander.grigoriev.121987@gmail.com>
2023-11-22 18:19:24 +0200

    implement Task 5

--- src/main/java/org/hw11task5/Main.java
+++ src/main/java/org/hw11task5/Main.java
@@ -2,13 +2,33 @@ package org.hw11task5;
 
 public class Main {
     public static void main(String[] args) {
-        MyThreadOne threadOne = new MyThreadOne();
-        MyThreadTwo threadTwo = new MyThreadTwo();
+        Object objA = new Object();
+        Object objB = new Object();
 
-        threadOne.t2 = threadTwo;
-        threadTwo.t1 = threadOne;
+        Thread t1 = new Thread("ThreadOne") {
+            public void run() {
+                synchronized (objA) {
+                    System.out.println("A is synchronized from " + Thread.currentThread().getName());
+                    synchronized (objB) {
+                        System.out.println("B is synchronized from " + Thread.currentThread().getName());
+                    }
+                }
+            }
+        };
 
-        threadOne.start();
-        threadTwo.start();
+        Thread t2 = new Thread("ThreadTwo") {
+            public void run() {
+                synchronized (objB) {
+                    System.out.println("B is synchronized from " + Thread.currentThread().getName());
+                    synchronized (objA) {
+                        System.out.println("A is synchronized from " + Thread.currentThread().getName());
+                    }
+                }
+            }
+        };
+
+        t1.start();
+        t2.start();
+
     }
 }
